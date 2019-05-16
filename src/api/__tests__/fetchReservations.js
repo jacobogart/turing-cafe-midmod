113cc90d8a73fe6931db276ebec489416e19ec2b
jacobogart <jacobogart@gmail.com>
2019-05-16 10:38:30 -0600

    Add fetchReservation tests

--- src/api/__tests__/fetchReservations.js
+++ src/api/__tests__/fetchReservations.js
@@ -1,3 +1,5 @@
+import { fetchReservations } from "../fetchReservations";
+
 describe('fetchReservations', () => {
   const url = "http://localhost:3001/api/v1/reservations";
   const mockReservation = {
@@ -7,12 +9,30 @@ describe('fetchReservations', () => {
     number: 1,
     id: 1
   }
+  const mockResList = [mockReservation, mockReservation];
   window.fetch = jest.fn().mockImplementation(() => Promise.resolve({
     ok: true,
-    json: () => Promise.resolve([mockReservation, mockReservation])
+    json: () => Promise.resolve(mockResList)
   }));
 
   it('should call fetch with the correct params', () => {
+    fetchReservations();
     expect(fetch).toBeCalledWith(url);
-  })
-})
+  });
+
+  it('should return an array of reservations', async () => {
+    const result = await fetchReservations();
+    expect(result).toEqual(mockResList);
+  });
+
+  it('should throw an error if the fetch fails', async () => {
+    window.fetch = jest.fn().mockImplementation(() =>
+      Promise.resolve({ ok: false }));
+
+    try {
+      await fetchReservations() 
+    } catch(error) {
+      expect(error.message).toEqual("Error fetching reservations");
+    }
+  });
+});
